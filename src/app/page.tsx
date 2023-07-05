59292c139145dbe963b3416f0b54319d51bfc8a7
Coding-Jarl <senvisage@gmail.com>
2023-07-05 22:53:15 +0200

    Add first & dirty stat/skill implementation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,53 @@
 'use client'
-import { Player, Character } from '../lib/rpg'
+import { useEffect, useState } from 'react'
+import { Character } from '../lib/rpg'
 
 export default function Home() {
-  const player1: Player = {
-    avatar: '',
-    mail: '',
-    name: 'ValhallaCode',
-  }
-  const guillaumec37 = new Character('Warthog')
-  const sevdoris = new Character('Héhé')
+  // useMemo ?
+  // KO par Guillaume: useRef
 
-  const hChangeStr = (evt)=>{
-    guillaumec37.setStr(evt.target.value)
-    console.log(guillaumec37.strBase)
+  // KO: Immer (classes POO)
+  // immutableJS ?
+
+  // KO: Variable à l'arrache
+  // const guillaumec37 = new Character('Warthog')
+
+  const [guillaumec37, setGuillaume] = useState<Character|null>(null)
+
+  useEffect(()=>{
+    setGuillaume(new Character('Warthog'))
+  }, [])
+
+  const hChange = (evt)=>{
+    if(!guillaumec37) return null
+
+    const newChar = new Character(guillaumec37.name, guillaumec37)
+    switch(evt.target.name){
+      case 'STR':
+        newChar.setStr(evt.target.value)
+        break;
+      case 'DEX':
+        newChar.setDex(evt.target.value)
+        break;
+      case 'CON':
+        newChar.setCon(evt.target.value)
+        break;
+      case 'INT':
+        newChar.setInt(evt.target.value)
+        break;
+      case 'WIS':
+        newChar.setWis(evt.target.value)
+        break;
+      case 'CHA':
+        newChar.setCha(evt.target.value)
+        break;
+                                    
+    }
+
+    setGuillaume(newChar)
   }
+
+  if(!guillaumec37) return null
 
   return (
     <main>
@@ -25,10 +59,48 @@ export default function Home() {
       <div className="Fluffs">Fluffs</div>
       <div className="Values">Values</div>
       <div className="Stats">
-        Stats
-        <input type="number" value={guillaumec37.strBase} onChange={hChangeStr} />
+        <h2>Stats</h2>
+        <label>
+          STR: 
+          <input type="number" name="STR" value={guillaumec37.strBase} onChange={hChange} />
+          ({guillaumec37.str()})
+        </label>
+        <label>
+          DEX: 
+          <input type="number" name="DEX" value={guillaumec37.dexBase} onChange={hChange} />
+          ({guillaumec37.dex()})
+        </label>
+        <label>
+          CON: 
+          <input type="number" name="CON" value={guillaumec37.conBase} onChange={hChange} />
+          ({guillaumec37.con()})
+        </label>
+        <label>
+          INT: 
+          <input type="number" name="INT" value={guillaumec37.intBase} onChange={hChange} />
+          ({guillaumec37.int()})
+        </label>
+        <label>
+          WIS: 
+          <input type="number" name="WIS" value={guillaumec37.wisBase} onChange={hChange} />
+          ({guillaumec37.wis()})
+        </label>
+        <label>
+          CHA: 
+          <input type="number" name="CHA" value={guillaumec37.chaBase} onChange={hChange} />
+          ({guillaumec37.cha()})
+        </label>
       </div>
-      <div className="Skills">Skills</div>
+      <div className="Skills">
+        <h2>Skills</h2>
+        {guillaumec37.skills.map( (elt)=>{
+          return <p key={elt.label}>
+            <input type="checkbox" disabled checked={elt.training}/>
+            {guillaumec37.skill(elt.label)} {elt.label} ({elt.stat})
+          </p>
+        } )}
+      
+      </div>
     </main>
   )
 }
--- src/lib/rpg.ts
+++ src/lib/rpg.ts
@@ -29,7 +29,7 @@ export class Character {
   chaBase: number
   
   currExpPoints:number=0
-  skills: Array<Skill>
+  skills:Array<Skill>=new Array<Skill>()
   miscProficiencies: Array<string>=["Language: Common", "Weapons: Common"]
   
   name: string
@@ -40,41 +40,148 @@ export class Character {
   bonds:Array<string>=[]
   flaws:Array<string>=[]
 
-  constructor(name: string) {
-    const d6 = new Die(6)
-    this.name = name
+  constructor(name: string, old?:Character) {
+    this.name = old?.name || name
 
-    this.strBase = d6.roll() + d6.roll() + d6.roll()
-    this.dexBase = d6.roll() + d6.roll() + d6.roll()
-    this.conBase = d6.roll() + d6.roll() + d6.roll()
-    this.intBase = d6.roll() + d6.roll() + d6.roll()
-    this.wisBase = d6.roll() + d6.roll() + d6.roll()
-    this.chaBase = d6.roll() + d6.roll() + d6.roll()
+    this.strBase = old?.strBase || 10
+    this.dexBase = old?.dexBase || 10
+    this.conBase = old?.conBase || 10
+    this.intBase = old?.intBase || 10
+    this.wisBase = old?.wisBase || 10
+    this.chaBase = old?.chaBase || 10
 
-    this.skills = new Array<Skill>()
+    if(old) {
+      this.skills = old.skills
+    } else {
+      this.skills.push({
+        label:"Acrobatics", 
+        stat:"DEX", 
+        training:false
+      })
+      this.skills.push({
+        label:"Animal Handling", 
+        stat:"WIS", 
+        training:false
+      })
+      this.skills.push({
+        label:"Arcana", 
+        stat:"INT", 
+        training:false
+      })
+      this.skills.push({
+        label:"Athletics", 
+        stat:"STR", 
+        training:false
+      })
+      this.skills.push({
+        label:"Deception", 
+        stat:"CHA", 
+        training:false
+      })
+      this.skills.push({
+        label:"History", 
+        stat:"INT", 
+        training:false
+      })
+      this.skills.push({
+        label:"Insight", 
+        stat:"WIS", 
+        training:false
+      })
+      this.skills.push({
+        label:"Initimidation", 
+        stat:"CHA", 
+        training:false
+      })
+      this.skills.push({
+        label:"Investigation", 
+        stat:"WIS", 
+        training:false
+      })
+      this.skills.push({
+        label:"Medicine", 
+        stat:"WIS", 
+        training:false
+      })
+      this.skills.push({
+        label:"Nature", 
+        stat:"INT", 
+        training:false
+      })
+      this.skills.push({
+        label:"Perception", 
+        stat:"WIS", 
+        training:false
+      })
+      this.skills.push({
+        label:"Performance", 
+        stat:"CHA", 
+        training:false
+      })
+      this.skills.push({
+        label:"Persuasion", 
+        stat:"WIS", 
+        training:false
+      })
+      this.skills.push({
+        label:"Religion", 
+        stat:"INT", 
+        training:false
+      })
+      this.skills.push({
+        label:"Sleight of Hand", 
+        stat:"DEX", 
+        training:false
+      })
+      this.skills.push({
+        label:"Stealth", 
+        stat:"DEX", 
+        training:false
+      })
+      this.skills.push({
+        label:"Survival", 
+        stat:"WIS", 
+        training:false
+      })
+    }
   }
 
   str() {
-    return (this.strBase - 10) / 2
+    return Math.floor((this.strBase - 10) / 2)
   }
   dex() {
-    return (this.dexBase - 10) / 2
+    return Math.floor((this.dexBase - 10) / 2)
   }
   con() {
-    return (this.conBase - 10) / 2
+    return Math.floor((this.conBase - 10) / 2)
   }
   int() {
-    return (this.intBase - 10) / 2
+    return Math.floor((this.intBase - 10) / 2)
   }
   wis() {
-    return (this.wisBase - 10) / 2
+    return Math.floor((this.wisBase - 10) / 2)
   }
   cha() {
-    return (this.chaBase - 10) / 2
+    return Math.floor((this.chaBase - 10) / 2)
   }
 
-  setStr(str:number){
-    this.strBase=str
+  setStr(newValue:number){
+    this.strBase=newValue
+  }
+  setDex(newValue:number){
+    this.dexBase=newValue
+  }
+  setCon(newValue:number){
+    this.conBase=newValue
+  }
+  setInt(newValue:number){
+    this.intBase=newValue
+  }
+  setWis(newValue:number){
+    this.wisBase=newValue
+  }
+  setCha(newValue:number){
+    this.chaBase=newValue
   }
   
   saveStr(){
